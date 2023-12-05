d4256d17516371988e9e57ce215aee33ee786d20
Erthium <ertyum35@gmail.com>
2023-12-05 18:30:25 +0300

    Update bridge_info function of solver

--- src/python3/solver.py
+++ src/python3/solver.py
@@ -38,6 +38,33 @@ def get_bridge_info(grid: list[list[Node]], x: int, y: int) -> dict[int, int]:
     assert grid[x][y].n_type == 1
     assert x >= 0 and x < len(grid)
     assert y >= 0 and y < len(grid[0])
+    output = {-1: 0, 0: 0, 1: 0, 2: 0, 3: 0}
+    for direction in [0, 1, 2, 3]:
+        dir_vector = direction_to_vector(direction)
+        check_x = x + dir_vector[0]
+        check_y = y + dir_vector[1]
+        if grid[check_x][check_y].n_type == 2:
+            if grid[check_x][check_y].b_dir % 2 == direction:
+                output[-1] += grid[check_x][check_y].b_thickness
+                continue
+        while check_x >= 0 and check_x < len(grid) and check_y >= 0 and check_y < len(grid[0]):
+            hit_node = grid[check_x][check_y]
+            if hit_node.n_type == 0:
+                check_x += dir_vector[0]
+                check_y += dir_vector[1]
+            elif hit_node.n_type == 1:
+                # Find the max possible input to the hit node
+                hit_node_max_input = 0
+                for hit_dir in [0, 1, 2, 3]:
+                    hit_side = direction_to_vector(hit_dir)
+                    if grid[check_x + hit_side[0]][check_y + hit_side[1]].n_type == 0:
+                        hit_node_max_input += 1
+                output[direction] = hit_node_max_input
+                break
+            elif hit_node.n_type == 2:
+                break
+
+
 
 def establish_bridge(grid: list[list[Node]], x: int, y: int, direction: int, thickness: int) -> None:
     assert grid[x][y].n_type == 1
